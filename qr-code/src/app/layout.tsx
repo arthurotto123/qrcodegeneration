408b5562b47dc62ca2dae709b5e1ba1c54596953
arthurotto123 <arthurottocoelho@gmail.com>
2025-05-25 21:33:03 -0300

    primeiro commit do projeto

---
+++ qr-code/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Poppins, Geist_Mono} from "next/font/google";
+import './styles.css';
+
+
+const poppings = Poppins({
+  weight: ["400", "500","600","700"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Gerador de QR CODE",
+  description: "Gerador de QR CODE",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={`${poppings}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ qr-code/src/app/page.tsx
@@ -0,0 +1,126 @@
+"use client";
+import { QRCodeCanvas, QRCodeSVG } from 'qrcode.react';
+import { FaUpload} from 'react-icons/fa';
+
+export default function Home() {
+  return (
+    <main className="container">
+      <section className="title-container">
+        <h1 className="page-title">Gere e customize  QR Codes
+          <span>dinâmicos</span>
+        </h1>
+
+        <img src="/arrow.svg" alt="setinha"
+          className="arrow-detail" />
+      </section>
+
+      <section className="qr-code-container">
+        <div className="qr-code">
+          <div className="link-input">
+            <label htmlFor="link">
+              Digite seu link
+            </label>
+          </div>
+          <input type="text"
+            id="link"
+            placeholder="Seu link aqui" />
+
+        </div>
+
+        <div className="qr-code-preview">
+          <p>
+            QR CODE PREVIEW
+          </p>
+
+          <QRCodeCanvas
+            value={"https://picturesofpeoplescanningqrcodes.tumblr.com/"}
+            title={"Title for my QR Code"}
+            size={128}
+            bgColor={"#ffffff"}
+            fgColor={"#000000"}
+            level={"L"}
+            imageSettings={{
+              src: "https://static.zpao.com/favicon.png",
+              x: undefined,
+              y: undefined,
+              height: 24,
+              width: 24,
+              opacity: 1,
+              excavate: true,
+            }} />
+        </div>
+        <div className="qr-code-customization">
+
+          <div className="customization-container">
+              <h3>
+                Cores
+              </h3>
+
+              <div className="input-container colors">
+                  <div className="input-box">
+                      <label htmlFor="fgColor">
+                        Cor principal
+                      </label>
+                      <input type="color"
+                      className='input-color'
+                      id='fgColor' />
+                  </div>
+
+                   <div className="input-box">
+                      <label htmlFor="bgColor">
+                        Cor do fundo
+                      </label>
+                      <input type="color"
+                      className='input-color'
+                      id='fgColor' />
+                  </div>
+
+
+              </div>
+          </div>
+
+          <div className="customization-container">
+              <h3>Logo</h3>
+
+               <div className="input-container">
+                  <div className="input-box">
+                      <label htmlFor="logo">
+                        Insira seu logo
+                      </label>
+                      <input type="file"
+                      className='input-file'
+                      id='logo'
+                      accept='image/*' />
+                  </div>
+
+                  <button className='input-file-button'>
+                       <FaUpload />
+                       Escolher arquivo
+                  </button>
+
+                   <div className="input-box">
+                      <label htmlFor="logoSize">
+                        Tamanho da logo
+                      </label>
+                      <select name="logoSize" id="logoSize">
+
+                        <option value="24"> 24px x 24px</option>
+                        <option value="38"> 38px x 38px</option>
+                        <option value="50"> 50px x 50px</option>
+                      </select>
+                  </div>
+
+
+              </div>
+
+          </div>
+
+        </div>
+
+        <button className="download-button">
+            Baixar QR Code
+        </button>
+      </section>
+    </main>
+  );
+}
